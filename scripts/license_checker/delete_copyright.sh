e473a83ab07096b92292b67400d52e2ffe34e4a2
IskandarKurbonov <iskandar.kurbonov@onlyoffice.com>
2024-04-26 16:40:49 +0500

    Add delte copyright

---
+++ scripts/license_checker/delete_copyright.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+REPO="document-server-integration/"
+
+search_and_replace() {
+    local file="$1"
+    perl -i -0777 -pe 's|/\*.*?Copyright Ascensio System SIA.*?\*/||gs' "$file"
+}
+
+find "$REPO" -type f \( -name "*.js" -o -name "*.java" -o -name "*.css" \) \
+  -not \( -path "$REPO/.git/*" -o -path "$REPO/.github/*" \) | while read -r file; do
+    if grep -q "Copyright Ascensio System SIA" "$file"; then
+        search_and_replace "$file"
+        echo "Removed copyright notice from: $file"
+        echo "$file" >> success.log
+    fi
+done
