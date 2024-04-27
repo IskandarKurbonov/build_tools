a0c5c61809a9b8b4aba675827cba97d512bf4392
IskandarKurbonov <iskandar.kurbonov@onlyoffice.com>
2024-04-27 17:00:24 +0500

    Add reuse

--- scripts/license_checker/delete_copyright.sh
+++ scripts/license_checker/delete_copyright.sh
@@ -2,15 +2,10 @@
 
 REPO="document-server-integration/"
 
-search_and_replace() {
-    local file="$1"
-    perl -i -0777 -pe 's|/\*.*?Copyright Ascensio System SIA.*?\*/||gs' "$file"
-}
-
 find "$REPO" -type f \( -name "*.js" -o -name "*.java" -o -name "*.css" \) \
   -not \( -path "$REPO/.git/*" -o -path "$REPO/.github/*" \) | while read -r file; do
     if grep -q "Copyright Ascensio System SIA" "$file"; then
-        search_and_replace "$file"
-        echo "$file" >> success.log
+        perl -i -0777 -pe 's|/\*.*?Copyright Ascensio System SIA.*?\*/||gs' "$file"
+        reuse annotate --year 2024 --license Apache-2.0 --copyright="Ascensio System SIA" "$file"
     fi
 done
